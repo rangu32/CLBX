4cefae8d6051cfe3e89d5d89b02b5ae7b0d054ed
Rangarajan 1 <rangarajan@celebratix.com>
2026-08-16 18:40:46 +0530

    Literals p1 & p2 Added

---
+++ src/ex_01_Java_Basics/Literals_P1.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Literals_P1 {
+
+        public static void main(String[] args) {
+            int age = 65;
+            // Literal - Integral
+
+            final float pi = 3.14f;
+            //  Literal - float
+
+
+            final int girl_age = 18;
+            //girl_age = 19;
+
+        }
+    }
---
+++ src/ex_01_Java_Basics/Literals_P2.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Literals_P2 {
+
+        public static void main(String[] args) {
+            float pi = 3.14f;
+            float pi2 = 3.14F;
+            float x = 10.00f;
+        }
+
+
+}
